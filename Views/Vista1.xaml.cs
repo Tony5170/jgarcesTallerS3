def757e3a8c39f699197165859a900f0caba07de
master <master@SMLAB306PC07_25>
2025-04-24 21:46:33 -0500

    Agregar archivos de proyecto.

---
+++ Views/Vista1.xaml.cs
@@ -0,0 +1,19 @@
+namespace jgarcesT3.Views;
+
+public partial class Vista1 : ContentPage
+{
+	public Vista1()
+	{
+		InitializeComponent();
+	}
+
+    private void txtidentificacion_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void btnGuardar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ Views/Vista2.xaml.cs
@@ -0,0 +1,9 @@
+namespace jgarcesT3.Views;
+
+public partial class Vista2 : ContentPage
+{
+	public Vista2()
+	{
+		InitializeComponent();
+	}
+}
